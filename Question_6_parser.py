db5685fdb3eda28d8598f2d8c6e9c38b3d9cadcd
Arthur Rakhmanov <artur.rakhmanov@south.rt.ru>
2021-05-24 15:01:20 +0300

    add sql question backbone

--- Question_6_parser.py
+++ Question_6_parser.py
@@ -5,3 +5,20 @@ connection_params_sql = {'host': '10.144.198.132',
                          'passwd': "Report_112233",
                          'charset': "utf8",
                          'db': "test"}
+
+
+# Создаем подключение
+con_serv = MySQLdb.connect(**connection_params_sql)
+# устанавливаем курсор
+cur_serv = con_serv.cursor()
+# Создаем таблицу
+create_query = ("CREATE TABLE IF NOT EXISTS `test`.`Anketa.Question6`")
+cur_serv._query(create_query)
+# Вставляем содержимое
+todb_insert_query = (f'INSERT INTO `Anketa.Question6`')
+cur_serv.execute(todb_insert_query)
+
+# #Закрываем соединение
+cur_serv.close()
+con_serv.commit()
+con_serv.close()
