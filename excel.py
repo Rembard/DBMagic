853be3e7c8566f9644be3ad56ee6c5934c23fc90
Arthur Rakhmanov <artur.rakhmanov@south.rt.ru>
2021-05-26 08:20:03 +0300

    remove old sources

--- excel.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import pandas as pd
-
-import xlrd
-import openpyxl
-path = "C:\\Users\\artur.rakhmanov\\Projects\\DBMagic\\try_to_import.xlsx"
-workbook = pd.read_excel(path)
-workbook.as_matrix()
