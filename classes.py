f77d33b8524fc274116933c8eb17055e869e6e55
Arthur Rakhmanov <artur.rakhmanov@south.rt.ru>
2021-06-10 09:07:39 +0300

    add classes

---
+++ classes.py
@@ -0,0 +1,59 @@
+# from numpy.lib.arraypad import pad
+import pandas as pd
+import re
+# from file_to_parse import *
+
+# file = "C:\\Users\\artur.rakhmanov\\Projects\\DBMagic\\Результаты_все_сотрудники.xlsx"
+class QuestionSlice():
+
+    def __init__(self, file, columns, header,skiprows):
+        self.file = file
+        self.columns = columns
+        self.header = header
+        self.skiprows = skiprows
+        self.filebody = pd.read_excel(self.file, usecols=self.columns, header=self.header, skiprows=self.skiprows)
+        
+    
+    def transquetobin(self):
+        """Конкатит одинаковые столбцы, меняет ответы на бинарные, обрезает нумерацию столбцов от pandas"""
+        column_names = self.filebody.columns.values.tolist()
+        for i in range(len(column_names)):
+            target_column = column_names[i]
+            for column in column_names:
+                if target_column.upper() == column[:-2].upper():
+                    self.filebody[target_column] = self.filebody[target_column].combine_first(self.filebody[column])
+                    del self.filebody[column]
+        # Удаляем лишние символы в заголовках столбцов
+        column_names = self.filebody.columns.values.tolist()                                 # Обновляем массив, так как могли быть удалены столбцы
+        for column in range(len(column_names)):                                              # Проверка по регулярке на окончания типа .[0-9$]
+            if re.search('\.[\d]$', column_names[column]):
+                column_names[column] = column_names[column][:-2]
+            column_names[column] = column_names[column][:64]                                 # Обрезаем имя столбца до 64 символов
+        self.filebody.columns = column_names
+        # Устанавливаем наличие ответов как 1, отсутствие - 0
+        self.filebody = (self.filebody.replace('.*',1,regex=True))
+        self.filebody = (self.filebody.fillna(0))
+        self.filebody = (self.filebody.astype(int))
+        return self.filebody
+
+    def createanswer(self):
+        """Возвращает датафрейм с ответами"""
+        return self.filebody
+
+    def createrpganswer(self):
+        """Возвращает датафрейм с ответами, преобразовав в Int"""
+        self.filebody = (self.filebody.fillna(0).astype(int))
+        return self.filebody
+
+    def concatcolumns(self):
+        for header in self.filebody.columns:
+            self.filebody.loc[self.filebody[header].notna(), header] = header
+        self.filebody = self.filebody.astype('str').replace("nan",'')
+        self.filebody = self.filebody.apply(lambda object: '\n'.join(list(filter(lambda a: a != '', object))), axis=1)
+        return self.filebody
+    # def mergechieflogins(self):
+    #     """Объединяет столбцец логинов со столбцом Источник ответа"""
+    #     dop_link = (pd.read_excel(input_file_chief, usecols='E',header=2, skiprows=0,names=['Источник ответа']))
+    #     dop_link['Источник ответа'] = dop_link['Источник ответа'].str[13:]
+    #     self.filebody = pd.concat([self.filebody,dop_link],axis=1)
+    #     return self.filebody
--- que_parser_as_tables.py
+++ que_parser_as_tables.py
@@ -2,57 +2,82 @@ from numpy import NaN, column_stack, inner
 import numpy as np
 import pandas as pd
 import re
+from classes import *
+from file_to_parse import *
 from sqlalchemy import create_engine
 from connection_params import db_creds,db_charset
-from file_to_parse import input_file,column_logins,column_question,column_questions_answer_rpg_style,column_questions_one_answer,table_name_example
 
 engine = create_engine(db_creds,encoding=db_charset)
-logins = (pd.read_excel(input_file, usecols=column_logins,header=2, skiprows=0,names=['logins']))          #Получаем логины
 
-def InsertQuestionIntoDB(question_coords, tablename):
-    question = (pd.read_excel(input_file, usecols=question_coords, header=2,skiprows=0))
-    # Объединяем одинаковые столбцы
-    items = question.columns.values.tolist()
-    for i in range(len(items)):
-        znak = items[i]
-        for item in items:
-            if znak.upper() == item[:-2].upper():
-                question[znak] = question[znak].combine_first(question[item])
-                del question[item]
-    # Удаляем лишние символы в заголовках столбцов
-    items = question.columns.values.tolist()                                                                        # Обновляем массив, так как могли быть удалены столбцы
-    for item in range(len(items)):                                                                              # Проверка по регулярке на окончания типа .[0-9$]
-        if re.search('\.[0-9]$', items[item]): 
-            items[item] = items[item][:-2]
-        items[item] = items[item][:64]                                                                          # Обрезаем имя столбца до 64 символов
-    question.columns = items
-    # Устанавливаем наличие ответов как 1, отсутствие - 0
-    question = (question.replace('.*',1,regex=True))
-    question = (question.fillna(0))
-    question = (question.astype(int))
-    # Склеиваем с логинами
-    frames = pd.concat([logins,question], axis=1).drop(index=[0])
-    # Кладём в бд, или ложим, или складываем, или слаживаем, пох
-    frames.to_sql(tablename,con=engine,if_exists='replace')
-    pass
+stuff_logins = (pd.read_excel(input_file_stuff, usecols=column_logins_stuff,header=2, skiprows=0,names=['logins']))          #Получаем логины
+# dop_link = (pd.read_excel(input_file, usecols='E',header=2, skiprows=0,names=['Источник ответа']))
+# dop_link['Источник ответа'] = dop_link['Источник ответа'].str[13:]
+# logins = pd.concat([logins,dop_link],axis=1)
+# print (logins)
 
-def InsertSingleAnswerQueIntoDB(question_coords, tablename):
-    single_answer_question = (pd.read_excel(input_file, usecols=question_coords, header=2,skiprows=0,names=['answer']))
-    result_table = pd.concat([logins,single_answer_question], axis=1).drop(index=[0])
-    result_table.to_sql(tablename,con=engine,if_exists='replace')
+def InsertQuestionIntoDB():
+    chief_logins = (pd.read_excel(input_file_chief, usecols=column_logins_chief,header=2, skiprows=0,names=['logins']))          #Получаем логины
+    for question in column_question_chief:
+        obj_question = DataFrame(input_file_chief,question[0], 2 ,0)
+        obj_question = pd.concat([chief_logins,obj_question.transquetobin()], axis=1).drop(index=[0])
+        table_name = table_name_chief_example+str(question[1])
+        obj_question.to_sql(table_name,con=engine,if_exists='replace')
+        print (table_name,' импортирована')
+
+def InsertSingleAnswerQueIntoDB():
+    chief_logins = (pd.read_excel(input_file_chief, usecols=column_logins_chief,header=2, skiprows=0,names=['logins']))          #Получаем логины
+    for question in column_question_chiefs_one_answer:
+        obj_question = DataFrame(input_file_chief,question[0], 2 ,0)
+        obj_question = pd.concat([chief_logins,obj_question.createanswer()], axis=1).drop(index=[0])
+        table_name = table_name_chief_example+str(question[1])
+        obj_question.to_sql(table_name,con=engine,if_exists='replace')
+        print (table_name, ' импортирована')
+#InsertQuestionIntoDB()
+InsertSingleAnswerQueIntoDB()
+#     question = (pd.read_excel(input_file, usecols=question_coords, header=2,skiprows=0))
+#     # Объединяем одинаковые столбцы
+#     items = question.columns.values.tolist()
+#     for i in range(len(items)):
+#         znak = items[i]
+#         for item in items:
+#             if znak.upper() == item[:-2].upper():
+#                 question[znak] = question[znak].combine_first(question[item])
+#                 del question[item]
+#     # Удаляем лишние символы в заголовках столбцов
+#     items = question.columns.values.tolist()                                                                        # Обновляем массив, так как могли быть удалены столбцы
+#     for item in range(len(items)):                                                                              # Проверка по регулярке на окончания типа .[0-9$]
+#         if re.search('\.[0-9]$', items[item]): 
+#             items[item] = items[item][:-2]
+#         items[item] = items[item][:64]                                                                          # Обрезаем имя столбца до 64 символов
+#     question.columns = items
+#     # Устанавливаем наличие ответов как 1, отсутствие - 0
+#     question = (question.replace('.*',1,regex=True))
+#     question = (question.fillna(0))
+#     question = (question.astype(int))
+#     # Склеиваем с логинами
+#     frames = pd.concat([logins,question], axis=1).drop(index=[0])
+#     # Кладём в бд, или ложим, или складываем, или слаживаем, пох
+#     frames.to_sql(tablename,con=engine,if_exists='replace')
+#     pass
+
+# def InsertSingleAnswerQueIntoDB(question_coords, tablename):
+#     single_answer_question = (pd.read_excel(input_file, usecols=question_coords, header=2,skiprows=0,names=['answer']))
+#     result_table = pd.concat([logins,single_answer_question], axis=1).drop(index=[0])
+#     result_table.to_sql(tablename,con=engine,if_exists='replace')
     
-for question in column_question:
-    table_name = table_name_example + str(question[1])
-    InsertQuestionIntoDB(question[0],table_name)
-    print(table_name,' импортирована.')
+# for question in column_question:
+#     table_name = table_name_example + str(question[1])
+#     InsertQuestionIntoDB(question[0],table_name)
+#     print(table_name,' импортирована.')
 
-for question in column_questions_one_answer:
-    table_name = table_name_example + str(question[1])
-    InsertSingleAnswerQueIntoDB(question[0],table_name)
-    print(table_name,' импортирована.')
+# for question in column_questions_one_answer:
+#     table_name = table_name_example + str(question[1])
+#     InsertSingleAnswerQueIntoDB(question[0],table_name)
+#     print(table_name,' импортирована.')
 
-rpg_style_questions = (pd.read_excel(input_file, usecols=column_questions_answer_rpg_style[0], header=2,skiprows=0))
-rpg_style_questions = (rpg_style_questions.fillna(0).astype(int))
-rpg_stype_table = pd.concat([logins,rpg_style_questions], axis=1).drop(index=[0])
-rpg_stype_table_name = table_name_example + str(column_questions_answer_rpg_style[1])
-rpg_stype_table.to_sql(rpg_stype_table_name,con=engine,if_exists='replace')
+# rpg_style_questions = (pd.read_excel(input_file, usecols=column_questions_answer_rpg_style[0], header=2,skiprows=0))
+# rpg_style_questions = (rpg_style_questions.fillna(0).astype(int))
+# rpg_stype_table = pd.concat([logins,rpg_style_questions], axis=1).drop(index=[0])
+# rpg_stype_table_name = table_name_example + str(column_questions_answer_rpg_style[1])
+# rpg_stype_table.to_sql(rpg_stype_table_name,con=engine,if_exists='replace')
+# print (chief_logins,'\n',stuff_logins)
